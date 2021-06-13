7b0463c2beb96febc2d71e5d38c267e94e5a542d
Avinesh <avineshpandey2101@gmail.com>
2021-06-14 01:07:16 +0530

    dp

--- DP/Coin_Combinations_I.cpp
+++ DP/Coin_Combinations_I.cpp
@@ -3,62 +3,49 @@ using namespace std;
 using ll = long long;
 #define mod 1000000007
 
-/*sieve of erathosthenes for primes from 1 to n*/  
-// vector<int>* sieve(){
-//         bool isPrime[MAX];
-
-//         vector<int>* primes = new vector<int>();   
-//         for(int i=2;i<=MAX;i++){
-//                 isPrime[i] = true;
-//         }
-//         for(int i=2;i*i<=MAX;i++){
-
-//                 if(isPrime[i]){
-//                         for(int j=i*i;j<=MAX;j+=i){
-//                                 isPrime[j] = false;
-//                         }
-//                 }
-//         }
-
-//         primes->pb(2);
-//         for(int i=3;i<=MAX;i+=2){
-//                 if(isPrime[i]){
-//                         primes->pb(i);
-//                 }
-//         }
-//         return primes;
-
-// }
-
-// void solve(ll t)
-// {
-
-// }
-
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
-
-    // vector<int>* primes = sieve();
-
-    // ll t;
-    // cin>>t;
-    // while(t--)
-    // {
-    //     solve(t);
-
-
-    // }
-    int a,b;
+    
+    ll a,b;//n,x
     cin>>a>>b;
-    int c[a];
+    ll c[a];
+    // int*c = new int[a+5];
     for(int i =0;i<a;i++)
     {
         cin>>c[i];
     }
-    cout<<"8";
+
+    // int*dp = new int[b+5];
+    ll dp[b+5];
+    // dp[b] = 8;
+    memset(dp,-1,sizeof(dp));
+    
+    dp[0] = 1;
+  
+    for(int i = 1;i<=b;i++)
+    {
+        ll ans=0;
+        for(int j=0;j<a;j++)
+        {
+            if( i-c[j] >= 0)
+            {
+                if(dp[i-c[j]] > -1)
+                {
+                    ans =( (ans % mod) + (dp[i-c[j]] % mod))%mod;
+                }
+            }
+        }
+        dp[i] = ans % mod;
+        ans = 0;
+    }
+
+    cout<<dp[b]<<endl;
+    // for(int i=0;i<b;i++)
+    // {
+    //     cout<<dp[i]<<" ";
+    // }
     return 0;
 }
--- DP/Coin_Combinations_II.cpp
+++ DP/Coin_Combinations_II.cpp
@@ -3,53 +3,10 @@ using namespace std;
 using ll = long long;
 #define mod 1000000007
 
-/*sieve of erathosthenes for primes from 1 to n*/  
-// vector<int>* sieve(){
-//         bool isPrime[MAX];
-
-//         vector<int>* primes = new vector<int>();   
-//         for(int i=2;i<=MAX;i++){
-//                 isPrime[i] = true;
-//         }
-//         for(int i=2;i*i<=MAX;i++){
-
-//                 if(isPrime[i]){
-//                         for(int j=i*i;j<=MAX;j+=i){
-//                                 isPrime[j] = false;
-//                         }
-//                 }
-//         }
-
-//         primes->pb(2);
-//         for(int i=3;i<=MAX;i+=2){
-//                 if(isPrime[i]){
-//                         primes->pb(i);
-//                 }
-//         }
-//         return primes;
-
-// }
-
-// void solve(ll t)
-// {
-
-// }
-
-
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
-
-    // vector<int>* primes = sieve();
-
-    // ll t;
-    // cin>>t;
-    // while(t--)
-    // {
-    //     solve(t);
-
-    // }
+    
     return 0;
 }
